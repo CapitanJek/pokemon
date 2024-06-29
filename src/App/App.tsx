c18dced2a338912f542045abd8c8a8536882bfa3
CapitanJek <azim.esenov.0@example.com>
2024-06-29 17:08:44 +0600

    add main

---
+++ src/App/App.tsx
@@ -0,0 +1,23 @@
+// import {useState} from "react";
+import '../style/index.css'
+function App() {
+    // const [pokemon, setPokemon] = useState({})
+    //
+    // const getPokemon =  async () => {
+    //     const response = await fetch("https://pokeapi.co/api/v2/pokemon/")
+    //     const data = await response.json()
+    //     console.log(data)
+    //     setPokemon(data)
+    // }
+
+  return (
+    <>
+        {/*<button onClick={getPokemon}>Start</button>*/}
+        <div className='wrapper'>
+            <h1>Hello world</h1>
+        </div>
+    </>
+  )
+}
+
+export default App
